6d45d36a568e3c983aae1234c8ecedae67b5c719
Yar Kravtsov <yarlson@gmail.com>
2016-09-30 23:05:47 +0300

    python 2.7 at setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Framework :: Django',
